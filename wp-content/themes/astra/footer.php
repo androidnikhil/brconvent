aa663f796471693ad3151358b84083e3e8745be8
nikhil-annalect <nikhil.pandey@annalect.com>
2025-06-26 12:30:20 +0530

    footer update

--- wp-content/themes/astra/footer.php
+++ wp-content/themes/astra/footer.php
@@ -1,36 +1,49 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Astra
- * @since 1.0.0
- */
+<footer class="site-footer">
+  <div class="footer-top text-dark pt-5 pb-5" style=" background: linear-gradient(to right, #00264b, #840404);">"&gt;
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 mt-5">
+          <img src="data_images/logo/logo-white.png" alt="Pooja Public School logo" class="footer-logo">
+          <div class="mt-4">
+            <p class="text-white" style="text-align: justify;">
+              BR Convent School is an English medium co-educational Senior Secondary School, affiliated with CBSE (10+2) under Bharat (Affiliation No. 2131311), offering a comprehensive educational experience from classes 1 through 12.
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 mt-5">
+          <h4 class="h5 text-white">CONTACT DETAILS</h4>
+          <div class="width-3rem bg-danger height-3 mt-3"></div>
+          <ul class="list-unstyled mt-4">
+            <li class="mb-3">
+              <i class="fa fa-phone mr-3 text-white"></i>
+              <a href="tel:+917454988851" class="text-white">+91 7454988851</a>
+            </li>
+            <li class="mb-3">
+              <i class="fa fa-map-marker mr-3 text-white"></i>
+              <span class="text-white">Nagla Kali Shashabad Road Agra- 282001</span>
+            </li>
+            <li class="mb-3">
+              <i class="fa fa-envelope mr-3 text-white"></i>
+              <a href="mailto:info@brconvent.com" class="text-white">info@brconvent.com</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 mt-5">
+          <h4 class="h5 text-white">QUICK LINKS</h4>
+          <div class="width-3rem bg-danger height-3 mt-3"></div>
+          <ul class="list-unstyled mt-4">
+            <li class="mb-2"><a href="about-us.php" class="text-white">About Us</a></li>
+            <li class="mb-2"><a href="contact_us.php" class="text-white">Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
+</footer>
+<div class="footer-bottom">
 
-?>
-<?php astra_content_bottom(); ?>
-	</div> <!-- ast-container -->
-	</div><!-- #content -->
-<?php
-	astra_content_after();
-
-	astra_footer_before();
-
-	astra_footer();
-
-	astra_footer_after();
-?>
-	</div><!-- #page -->
-<?php
-	astra_body_bottom();
-	wp_footer();
-?>
-	</body>
-</html>
+  <p class="mb-0">
+    ©2025 <span class="text-danger">BR CONVENT SCHOOL</span>. All rights reserved. 
+  </p>
+</div>
