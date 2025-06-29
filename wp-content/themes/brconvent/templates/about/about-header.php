37616e97bc056c7c1f5c959c606747b8f9b186b8
nikhil-annalect <nikhil.pandey@annalect.com>
2025-06-29 19:15:42 +0530

    header update

--- wp-content/themes/brconvent/templates/about/about-header.php
+++ wp-content/themes/brconvent/templates/about/about-header.php
@@ -1,6 +1,6 @@
 <div class="relative h-100 sm:h-80">
             <img class="absolute inset-0 w-full h-full object-cover" src="/wp-content/uploads/2025/06/about_header-scaled.png" alt="School building facade">
-            <div class="absolute inset-0 bg-school-navy bg-opacity-60"></div>
+            <div class="absolute inset-0 bg-primary-blue bg-opacity-60"></div>
             <div class="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 h-full flex items-center justify-center">
                 <h1 class="text-4xl sm:text-5xl md:text-6xl font-extrabold text-white text-center font-montserrat">About Our School</h1>
             </div>
--- wp-content/themes/brconvent/templates/admission/header.php
+++ wp-content/themes/brconvent/templates/admission/header.php
@@ -1,5 +1,5 @@
  <div class="relative h-64 sm:h-80">
-            <img class="absolute inset-0 w-full h-full object-cover" src="https://placehold.co/1600x400/FFD700/003366?text=Join+Our+Family" alt="Happy students in uniform">
+            <img class="absolute inset-0 w-full h-full object-cover" src="https://brconventpublicschool.com/wp-content/uploads/2025/06/admission.png" alt="Happy students in uniform">
             <div class="absolute inset-0 bg-primary-blue bg-opacity-60"></div>
             <div class="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 h-full flex items-center justify-center">
                 <h1 class="text-4xl sm:text-5xl md:text-6xl font-extrabold text-white text-center font-montserrat">Admissions</h1>
